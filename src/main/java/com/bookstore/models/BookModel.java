aa3a39fb5b13f6fe9995e6da92a6633c38927a3b
Leeflorencio <florenciolee63@gmail.com>
2024-07-17 02:01:23 -0300

    criando os relacionamentos entre as models

--- src/main/java/com/bookstore/models/BookModel.java
+++ src/main/java/com/bookstore/models/BookModel.java
@@ -3,6 +3,8 @@ package com.bookstore.models;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
 @Entity
@@ -16,6 +18,34 @@ public class BookModel implements Serializable {
 
     @Column(nullable = false, unique = true)
     private String title;
+
+    //@JsonProperty(acess = JsonProperty.Acess.WRITE_ONLY)
+    // @ManyToOne //(fetch = FetchType.LAZY)
+    @JoinColumn(name = "publisher_id")
+    private PublisherModel publisher;
+
+    @ManyToMany
+    @JoinTable(
+            name = "book_author",
+            joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id"))
+    private Set<AuthorModel> authors = new HashSet<>();
+
+    public Set<AuthorModel> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<AuthorModel> authors) {
+        this.authors = authors;
+    }
+
+    public PublisherModel getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(PublisherModel publisher) {
+        this.publisher = publisher;
+    }
 
     public UUID getId() {
         return id;
